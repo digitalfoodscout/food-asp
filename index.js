55f0a9d7a0b2e1bc5076e2266d8cbd9a63bb9657
Daniel Sendzik <d.sendzik@gmail.com>
2017-05-31 14:11:40 +0200

    Gewichtung hinzugefügt (testweise)

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const data = {
             date: moment("2017-05-24 09:30")
         },
         {
-            type: "lactose",
+            type: "viel_lactose",
             date: moment("2017-05-24 11:20")
         },
         {
@@ -20,28 +20,70 @@ const data = {
         {
             type: "bauchschmerzen",
             date: moment("2017-05-24 09:50")
+        },
+        {
+            type: "bauchschmerzen",
+            date: moment("2017-05-24 11:30")
         }
     ]
 };
 
 const numLactose = data.ate.reduce((acc, val) => val.type === "lactose" ? acc+1 : acc, 0);
 
-const rules = [
-    `intolerance(lactose) :- ate(1..${numLactose}, lactose), symptom(1..${numLactose}, bauchschmerzen), not -intolerance(lactose).`,
-    `-intolerance(lactose) :- ate(1..${numLactose}, lactose), not symptom(1..${numLactose}, bauchschmerzen).`
-];
+const rules = [];
+
+let bigRule = "lactose :- #sum {  ";
 
 data.ate.forEach((ate, index) => {
-   rules.push(`ate(${index}, ${ate.type}).`);
+    const ateRule = `ate(${index}, ${ate.type})`;
+   rules.push(ateRule + ".");
 
-   data.symptoms.forEach(symptom => {
+    let ateWeight = 0;
+
+    switch(ate.type) {
+        case "viel_lactose":
+            ateWeight = -15;
+            break;
+        case "wenig_lactose":
+            ateWeight = -5;
+            break;
+        case "lactose":
+            ateWeight = -10;
+            break;
+    }
+
+    bigRule += `${ateWeight},${ateRule} : ${ateRule}; `;
+
+    data.symptoms.forEach(symptom => {
       const difference = symptom.date.diff(ate.date);
 
       if(difference > 0 && difference <= 45 * 60 * 1000) {
-          rules.push(`symptom(${index}, ${symptom.type}).`);
+          const symptRule = `symptom(${index}, ${symptom.type})`;
+          rules.push(symptRule + ".");
+
+          let symptWeight = 0;
+
+          switch(symptom.type) {
+              case "bauchschmerzen":
+                  symptWeight = Math.abs(ateWeight * 2);
+                  break;
+              case "kopfschmerzen":
+                  symptWeight = Math.abs(ateWeight);
+                  break;
+          }
+
+          bigRule += `${symptWeight},${symptRule} : ${symptRule}; `;
       }
    });
 });
+
+
+bigRule = bigRule.slice(0,-2);
+bigRule += `} > 0.`;
+
+rules.push(bigRule);
+
+//console.log(rules.join('\n'));
 
 aspConnect.runASPSolver(rules).then(models => {
     console.log(models);
