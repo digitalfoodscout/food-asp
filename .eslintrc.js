78e5f8c4aeb230720c08a68d4e0e4ac18ea4a071
Daniel Sendzik <d.sendzik@gmail.com>
2017-06-01 12:30:44 +0200

    Editorconfig und ESLint hinzugefügt

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2017
+  },
+  extends: 'eslint:recommended',
+  env: {
+    mocha: true,
+    node: true
+  },
+  rules: {
+  }
+};
