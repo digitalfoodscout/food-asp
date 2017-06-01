387a081dd89839571d09de8d447c774b6b110224
Daniel Sendzik <d.sendzik@gmail.com>
2017-06-01 12:31:49 +0200

    Added symptom-strength and amount

--- index.js
+++ index.js
@@ -1,92 +1,101 @@
-const aspConnect = require('clingo-connect');
-const moment = require('moment');
+const aspConnect = require('clingo-connect')
+const moment = require('moment')
 
 const data = {
-    ate: [
-        {
-            type: "lactose",
-            date: moment("2017-05-24 09:30")
-        },
-        {
-            type: "viel_lactose",
-            date: moment("2017-05-24 11:20")
-        },
-        {
-            type: "fructose",
-            date: moment("2017-05-24 11:30")
-        }
-    ],
-    symptoms: [
-        {
-            type: "bauchschmerzen",
-            date: moment("2017-05-24 09:50")
-        },
-        {
-            type: "bauchschmerzen",
-            date: moment("2017-05-24 11:30")
-        }
-    ]
-};
+  ate: [
+    {
+      type: 'lactose',
+      amount: 'few',
+      date: moment('2017-05-24 09:30')
+    },
+    {
+      type: 'lactose',
+      amount: 'much',
+      date: moment('2017-05-24 11:20')
+    },
+    {
+      type: 'fructose',
+      amount: 'normal',
+      date: moment('2017-05-24 11:30')
+    }
+  ],
+  symptoms: [
+    {
+      type: 'bauchschmerzen',
+      strength: 'low',
+      date: moment('2017-05-24 09:50')
+    },
+    {
+      type: 'bauchschmerzen',
+      strength: 'medium',
+      date: moment('2017-05-24 11:30')
+    }
+  ]
+}
 
-const numLactose = data.ate.reduce((acc, val) => val.type === "lactose" ? acc+1 : acc, 0);
+const rules = []
 
-const rules = [];
-
-let bigRule = "lactose :- #sum {  ";
+let sumRule = 'lactose :- #sum {  '
 
 data.ate.forEach((ate, index) => {
-    const ateRule = `ate(${index}, ${ate.type})`;
-   rules.push(ateRule + ".");
+  const ateRule = `ate(${index}, ${ate.type})`
+  rules.push(ateRule + '.')
 
-    let ateWeight = 0;
+  let ateWeight = 0
 
-    switch(ate.type) {
-        case "viel_lactose":
-            ateWeight = -15;
-            break;
-        case "wenig_lactose":
-            ateWeight = -5;
-            break;
-        case "lactose":
-            ateWeight = -10;
-            break;
-    }
-
-    bigRule += `${ateWeight},${ateRule} : ${ateRule}; `;
-
-    data.symptoms.forEach(symptom => {
-      const difference = symptom.date.diff(ate.date);
-
-      if(difference > 0 && difference <= 45 * 60 * 1000) {
-          const symptRule = `symptom(${index}, ${symptom.type})`;
-          rules.push(symptRule + ".");
-
-          let symptWeight = 0;
-
-          switch(symptom.type) {
-              case "bauchschmerzen":
-                  symptWeight = Math.abs(ateWeight * 2);
-                  break;
-              case "kopfschmerzen":
-                  symptWeight = Math.abs(ateWeight);
-                  break;
-          }
-
-          bigRule += `${symptWeight},${symptRule} : ${symptRule}; `;
+  switch (ate.type) {
+    case 'lactose':
+      switch (ate.amount) {
+        case 'much':
+          ateWeight = -15
+          break
+        case 'few':
+          ateWeight = -5
+          break
+        case 'normal':
+          ateWeight = -10
+          break
       }
-   });
-});
+      break;
+    case 'fructose':
+      // TODO: Implement
+      break;
+  }
 
+  sumRule += `${ateWeight},${ateRule} : ${ateRule}; `
 
-bigRule = bigRule.slice(0,-2);
-bigRule += `} > 0.`;
+  data.symptoms.forEach(symptom => {
+    const difference = symptom.date.diff(ate.date)
 
-rules.push(bigRule);
+    if (difference > 0 && difference <= 45 * 60 * 1000) {
+      const symptRule = `symptom(${index}, ${symptom.type})`
+      rules.push(symptRule + '.')
 
-//console.log(rules.join('\n'));
+      let symptWeight = 0
+
+      switch (symptom.type) {
+        case 'bauchschmerzen':
+          symptWeight = Math.abs(ateWeight * 2)
+          break
+        case 'kopfschmerzen':
+          symptWeight = Math.abs(ateWeight)
+          break
+      }
+
+      sumRule += `${symptWeight},${symptRule} : ${symptRule}; `
+    }
+  })
+})
+
+sumRule = sumRule.slice(0, -2)
+sumRule += `} > 0.`
+
+rules.push(sumRule)
+
+//console.log(rules.join('\n'))
 
 aspConnect.runASPSolver(rules).then(models => {
-    console.log(models);
+  console.log(models)
 }).catch(err => {
-    console.log(err);
-});
+  console.log(err)
+})
